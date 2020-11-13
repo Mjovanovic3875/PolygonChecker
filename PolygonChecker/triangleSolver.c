9d869d9bac89a974d17cd07c702cceb48205aff8
techee12 <70961125+techee12@users.noreply.github.com>
2020-11-13 12:14:06 -0500

    Tests for triangleSolver created

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -12,7 +12,7 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 		result = "Equilateral triangle";
 	}
 	else if ((side1 == side2 && side1 != side3) || // was missing if side 2 and 3 are equal 
-		(side1 == side3 && side1 != side2) || (side2 == 3 && side2 != side1))
+		(side1 == side3 && side1 != side2) || (side2 == side3 && side2 != side1))
 	{
 		result = "Isosceles triangle";
 	}
--- Unit_Tests/Unit_Tests.cpp
+++ Unit_Tests/Unit_Tests.cpp
@@ -13,9 +13,39 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(INVALID_TRAINGLE_ZERO_NEGITIVE)
 		{
-		
+			Assert::AreEqual(analyzeTriangle(0,0,0),"Not a triangle");
+			Assert::AreEqual(analyzeTriangle(0, 1, 0), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(1, 0, 0), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(1, 1, 0), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(1, 1, -1), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(1, -1, 1), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(-1, 1, 1), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(-1, -1, -1), "Not a triangle");
+		}
+		TEST_METHOD(INVALID_TRIANGLE_CHAR) {
+			Assert::AreEqual(analyzeTriangle(0, 0, 'a'), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle(0, 'b', 0), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle('c', 0, 0), "Not a triangle");
+			Assert::AreEqual(analyzeTriangle('c', 'b', 'a'), "Not a triangle");
+		}
+
+		TEST_METHOD(ISOSCELES) {
+			Assert::AreEqual(analyzeTriangle(1, 1, 2), "Isosceles triangle");
+			Assert::AreEqual(analyzeTriangle(1, 2, 1), "Isosceles triangle");
+			Assert::AreEqual(analyzeTriangle(2, 1, 1), "Isosceles triangle");
+	
+		}
+		TEST_METHOD(EQUALATERAL) {
+			for (int i = 1; i < 200; i++) {
+				Assert::AreEqual(analyzeTriangle(i, i, i), "Equilateral triangle");
+			}
+		}
+		TEST_METHOD(SCALENE) {
+			for (int i = 1; i < 200; i++) {
+				Assert::AreEqual(analyzeTriangle(i, i + 1, i + 2), "Scalene triangle");
+			}
 		}
 	};
 }

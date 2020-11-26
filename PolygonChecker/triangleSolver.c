690ff568fd94bff875a94bb0a667bacf8f9e271a
Emil Harvey <Eharvey3704@conestogac.on.ca>
2020-11-26 08:41:35 -0500

    Implemented insideAngles(). Also implemented an findAngle() as a helper.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdbool.h>
 
 #include "triangleSolver.h"
@@ -19,6 +20,26 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	else {
 		result = "Scalene triangle";
 	}
+
+	return result;
+}
+
+
+double findAngle(int a, int b, int c)
+{
+	double numerator = pow(b, (double)2) + pow(c, (double)2) - pow(a, (double)2);
+	double denominator = (double)2 * (double)b * (double)c; // explicit cast upwards to avoid  overflow (hopefully)
+	return acos(numerator / denominator);
+}
+
+
+double* insideAngles(int side1, int side2, int side3)
+{
+	// See https://www.calculator.net/triangle-calculator.html#:~:text=The%20interior%20angles%20of%20a,of%20interest%20from%20180%C2%B0.
+	double* result = (double*)malloc(sizeof(double) * 3);
+	result[0] = findAngle(side1, side2, side3);
+	result[1] = findAngle(side2, side1, side3);
+	result[2] = findAngle(side3, side1, side2);
 
 	return result;
 }

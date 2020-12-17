1de40aa3a98ec094286be26ccafb09ee6a12e232
Emil Harvey <Eharvey3704@conestogac.on.ca>
2020-12-17 00:42:20 -0500

    Removed old test project

--- PolygonChecker.tests/PolygonChecker.tests.cpp
+++ PolygonChecker.tests/PolygonChecker.tests.cpp
@@ -1,3 +1,26 @@
+/*
+Week 14 Program 1 Assignment : Polygon Checker Group Assignment
+Danny A. Smith, Emil Harvey, Milan Jovanovic
+PROG71985- Fall 2020
+December 2020
+
+Revision History
+
+1.0	NOVEMBER	 Files Created
+1.1	NOVEMBER	 Triangle solver updated
+1.2	NOVEMBER	 Inside angles functions completed
+1.3	DECEMBER	 Tests written and triangle solver refactored
+1.4	DECEMBER	 Get points from user function completed
+1.5	December	 Get sides from points function completed
+1.6	DECEMBER	 Refactored rectangle Checker
+1.7	DECEMBER	 Refactored function names
+1.8	DECEMBER	 Tests broken down into classes
+
+Program Description :
+	This program is designed to check shapes. It can currently take in triangle lines and determin the type of triangle and angles. It can also
+	take in rectangle coordinates and decide if it is a rectangle or not. Future upgradability extends to checking other polygons.
+*/
+
 #include "pch.h"
 #include "CppUnitTest.h"
 
